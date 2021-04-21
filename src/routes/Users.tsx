01a0aae80a8784739875e6a29bf7084bceb3d733
Johan Mowitz <johan@arthro.ai>
2021-04-21 14:05:46 +0200

    Adds gender filter in user list

--- src/routes/Users.tsx
+++ src/routes/Users.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState }  from 'react'
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
 
 const testUsers = [
-  { id: 1, firstName: "Clark", lastName: "Kent" },
-  { id: 2, firstName: "Bruce", lastName: "Wayne" },
+  { id: 1, firstName: "Clark", lastName: "Kent", gender: "male" },
+  { id: 2, firstName: "Bruce", lastName: "Wayne", gender: "male" },
+  { id: 3, firstName: "Jessica", lastName: "Jones", gender: "female" },
 ]
 
 const Users = () => {
-  const [users] = useState(testUsers)
+  const { register, watch } = useForm()
+  const watchGender = watch("gender", "female");
+  const [filteredUsers, setFilteredUsers] = useState(testUsers.filter((user) => user.gender === watchGender))
+
+  useEffect(() => {
+    setFilteredUsers(testUsers.filter((user) => user.gender === watchGender))
+  },[watchGender])
 
   return (
-    <ul>
-      {users.map((user) => { return ( <li><Link to={`/user/${user.id}`}>{`${user.firstName} ${user.lastName}`}</Link></li> )})}
-    </ul>
+    <div>
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+      </select>
+
+      <ul>
+        {filteredUsers.map((user) => { return ( <li><Link to={`/user/${user.id}`}>{`${user.firstName} ${user.lastName}`}</Link></li> )})}
+      </ul>
+    </div>
   );
 }
 
