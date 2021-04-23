b748565c9e54532bf9cb0a57b8e7acb2450a8a85
Joao Correia <joao@hedy.tech>
2021-04-23 14:24:47 +0200

    Adds getUser dummy method to api

--- src/service/api.ts
+++ src/service/api.ts
@@ -3,7 +3,10 @@ import apiLib from '../lib/api'
 const api = {
     getUsers: async () => {
         return apiLib.getUsUsers();
-    }
+    },
+    getUser: async (id: string) => {
+
+    },
 }
 
 export default api
