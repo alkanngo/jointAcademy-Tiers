d9f45a55ba40dc20175120b89ee558c29c91b8e4
Joao Correia <joao@hedy.tech>
2021-04-27 10:53:17 +0200

    Adds fetching solution that prevents blocking

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,48 @@
 import apiLib from '../lib/api'
-import user from './user'
+import user,{ User } from './user'
+
+// type userRecord = Record<string, any>;
+interface userUpdateEvent {
+    success: boolean
+    market: string
+    newUsers?: User[]
+    users: User[]
+}
+type replyEventListener = (event: userUpdateEvent) => void
 
 const api = {
+    /**
+     * This will allow to install an function to be called when a new reply has happened
+     */
+    getUsersSolA: (eventListener: replyEventListener = () => undefined) => {
+        let users: User[] = [];
+        
+        const addNewUsers = (newData: User[]) => {
+            users = [
+                ...users,
+                ...newData,
+            ]    
+        }
+
+        const dispatch = (success: boolean, market: string, newUsers: User[] = []) => {
+            addNewUsers(newUsers)
+            eventListener({
+                success,
+                market,
+                newUsers,
+                users
+            });
+        }
+
+        apiLib.getUsUsers()
+            .then(records => dispatch(true, 'US', records.map(usUser => user.usUserToUser(usUser))))
+            .catch(() => dispatch(false, 'US'))
+
+        apiLib.getFiUsers()
+            .then(records => dispatch(true, 'FI', records.map(fiUser => user.fiUserToUser(fiUser))))
+            .catch(() => dispatch(false, 'FI'))
+    },
+
     getUsers: async () => {
       const fiUsers = await apiLib.getFiUsers()
       const usUsers = await apiLib.getUsUsers()
