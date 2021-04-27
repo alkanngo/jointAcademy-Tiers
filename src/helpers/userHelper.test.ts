50a02f37bd5860cef91cbd968529725875cc1a86
Johan Mowitz <johan@arthro.ai>
2021-04-27 14:50:39 +0200

    Updates getTier test case

--- src/helpers/userHelper.test.ts
+++ src/helpers/userHelper.test.ts
@@ -1,6 +1,45 @@
-import userHelper from './userHelper'
+import userHelper, { User } from './userHelper'
 
-test('tiers', () => {
-  // TODO: Mock Date
-  expect(userHelper.getTier({ registered: { date: '2006-07-05T13:10:42.010Z' }})).toEqual('GOLD')
-});
+const testUser: User = {
+  cell: '',
+  email: '',
+  gender: '',
+  id: '',
+  market: '',
+  name: {
+    first: '',
+    last: '',
+    title: '',
+  },
+  phone: '',
+  picture: {
+    large:  '',
+    medium:  '',
+    thumbnail:  '',
+  },
+  registrationDate: '',
+  userName: '',
+}
+
+describe("tiers", () => {
+  beforeAll(() => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date(2021, 4, 27));
+  });
+  
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test('gold', () => {
+    expect(userHelper.getTier({ ...testUser, registrationDate: '2006-07-05T13:10:42.010Z' })).toEqual('GOLD')
+  })
+
+  test('silver', () => {
+    expect(userHelper.getTier({ ...testUser, registrationDate: '2015-07-05T13:10:42.010Z' })).toEqual('SILVER')
+  })
+
+  test('bronze', () => {
+    expect(userHelper.getTier({ ...testUser, registrationDate: '2019-07-05T13:10:42.010Z' })).toEqual('BRONZE')
+  })
+})
