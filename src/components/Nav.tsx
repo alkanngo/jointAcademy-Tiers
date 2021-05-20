20ef3b9e195f33d5ab57fd66c4223c340685d9db
al <aladdin.kanngo@hotmail.com>
2021-05-20 20:59:55 +0200

    Add Nav component and scss
    Set background color and spacing reset in index.css

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import  auth  from "./../service/auth"
+import "./Nav.scss"
 
 const Nav = () => {
   const { getUsername } = auth
+  const testUser = getUsername();
 
 
 
   return (
-    <div>
-      <h3>{getUsername()}</h3>
+    <div className="container">
+      <div className="name_container">
+        <h2 className="site_name">Check <span className="silver">Your </span> <span className="gold"> Tier </span></h2>
+      </div>
+      <div className="profile_pic">
+        <img alt="profile" src="https://i.redd.it/9qvwe2ju0ch41.png"/>
+        <h3>Logged in as: {getUsername()}</h3>
+      </div>
     </div>
   );
 }
