4adc7ce0d80501ce2ee0ca5b0a1f1fae27851dff
al <aladdin.kanngo@hotmail.com>
2021-05-25 15:55:51 +0200

    Add a .scss file to UserList and also provide logic
    The logic includes to render only specified tiers when choosen , see who the testUser is and also provide the illustration component when list is not populated

---
+++ src/routes/UserList/UserList.tsx
@@ -0,0 +1,94 @@
+/**
+ * The candidate may change this file contents
+ */
+import React, { useEffect, useState }  from 'react'
+import { useHistory } from "react-router-dom"
+import apiService from "../../service/api"
+import userService, { User } from "../../service/user"
+import  auth  from "../../service/auth"
+import ButtonGroup from "../../components/ButtonGroup/ButtonGroup"
+import Illustration from '../../components/Illustration/Illustration'
+import "./UserList.scss" 
+
+
+const UserList = () => {
+  const { getTier } = userService
+  const { getUserId } = auth
+  const history = useHistory();
+  const [users, setUsers] = useState<User[]>([])
+  const [testUser, setTestUser] = useState<User>()
+  const [selectedTier, setSelectedTier] = useState<string>()
+  const [listPopulated, setListPopulated] = useState<boolean>(false)
+
+  useEffect(() => {
+    const { result: users } = apiService.getUsers()
+    if (users) {
+        users.then((userRecords) => {
+          setUsers(userRecords)
+        })
+        getTestUser()
+    }
+  }, [])
+
+  const navigateToDetails = (id: string, user: User) => {
+    history.push({pathname:`/user/${id}`, state: {user: user,}})
+  }
+
+  const getTestUser = () => {
+    const testUserId = getUserId()
+    users.forEach(user => {
+      if(testUserId === user.login.id){
+        return setTestUser(user)
+      }
+    })
+  }
+  const renderSelectedTier = (event: React.ChangeEvent<HTMLButtonElement>) => {
+    setSelectedTier(event.target.name)
+    setListPopulated(true)
+  };
+
+  return (
+    <main className="nav__list">
+      <div className="btn-group">
+        <ButtonGroup 
+          buttons={["BRONZE", "SILVER", "GOLD"]}
+          selectedButton={renderSelectedTier}
+        />
+      </div>
+      {!listPopulated
+        ? <Illustration />
+        :<div className="list">
+          <table className="list__table">
+            <thead className="table__head">
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Market</th>
+                <th>Tier</th>
+              </tr>
+            </thead>
+            <tbody className="table__body">
+            {users.map((user, i) => (
+              user.nationality === "US" && getTier(user) === selectedTier
+              ? <tr 
+                  key={i} 
+                  onClick={() => navigateToDetails(user.login.uuid, user)} 
+                  className={getTier(user).toLowerCase()}
+                  style={{cursor: "pointer"}}
+                >
+                  <td><img src={user.picture.thumbnail} alt="avatar" /></td>
+                  <td>{`${user.name.title} ${user.name.first} ${user.name.last}`}</td>
+                  <td>{`${user.nationality}`}</td>
+                  <td>{`${getTier(user)}`}</td>
+                </tr>
+              : <></>
+            ))}
+          </tbody>
+        </table>
+      </div>
+        }
+    </main>
+  );
+}
+
+export default UserList
