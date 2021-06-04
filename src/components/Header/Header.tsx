fed33dda507201c2feff9ee36aa827a00d741c8c
al <aladdin.kanngo@hotmail.com>
2021-06-04 14:52:10 +0200

    Add instance of contexthook to render on to Header
    Add links to home and to testUser detail page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import  auth  from "../../service/auth"
+import React from "react"
+import { User } from "../../service/user"
+import { useAppContext } from "../../context/Context"
+import { Link } from "react-router-dom"
 import "./Header.scss"
 
-const Header: React.FunctionComponent = () => {
-  const { getUsername } = auth
+const Header: React.FC = () => {
+  const ctxVal = useAppContext()
+  const testUser: User = ctxVal.testUser
 
   return (
     <header className="header">
-      <div className="header__title">
-        <h2 className="site_name">Check <span className="silver">Your </span> <span className="gold"> Tier </span></h2>
-      </div>
-        <p className="user">Logged in as: {getUsername()}</p>
+      <Link to="/" className="home__link">
+        <div className="title">
+          <h2 className="site_name">Check 
+            <span className="silver"> Your </span>
+            <span className="gold"> Tier </span>
+          </h2>
+        </div>
+      </Link>
+      <Link className="testUser__link" to={{
+        pathname: `/user/${testUser.login.uuid}`,
+        state: { user: testUser }
+      }}>
+        <div className="testUser" >
+          <img 
+            src={testUser.picture.medium} 
+            alt="Current User" 
+            className="testUser__img" />
+          <p className="testUser__username">Logged in as: {testUser.login.username}</p>
+        </div>
+      </Link>
     </header>
   );
 }
